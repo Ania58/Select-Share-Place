6ca30c91ba23cbb1cb970265b20ec688acccd7bf
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-05-25 19:41:05 +0200

    Add Google Maps Geocoding API request with secure API key handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 const apiKey = process.env.GOOGLE_MAPS_API_KEY;
 
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+    results: {geometry: {location: {lat: number, lng: number}}}[];
+    status: 'OK' | 'ZERO_RESULTS';
+}
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
+
+    axios.get<GoogleGeocodingResponse>
+    (`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${apiKey}`)
+    .then(response => {
+        if (response.data.status !== 'OK') {
+            throw new Error('Could not fetch location!');
+        }
+        const coordinates = response.data.results[0].geometry.location;
+    }).catch(err => {
+        alert(err.message);
+        console.log(err);
+    })
 }
 
 form?.addEventListener('submit',searchAddressHandler);
